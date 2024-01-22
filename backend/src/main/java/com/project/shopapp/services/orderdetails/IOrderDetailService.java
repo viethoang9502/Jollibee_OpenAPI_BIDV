909aea114d522f3486bf813aac8621df13c8ac6a
Võ Việt Hoàng <145230873+viethoang9502@users.noreply.github.com>
2024-01-22 18:10:24 +0700

    'update_fontend'

--- backend/src/main/java/com/project/shopapp/services/orderdetails/IOrderDetailService.java
+++ backend/src/main/java/com/project/shopapp/services/orderdetails/IOrderDetailService.java
@@ -8,11 +8,14 @@ import java.util.List;
 
 public interface IOrderDetailService {
     OrderDetail createOrderDetail(OrderDetailDTO newOrderDetail) throws Exception;
+
     OrderDetail getOrderDetail(Long id) throws DataNotFoundException;
+
     OrderDetail updateOrderDetail(Long id, OrderDetailDTO newOrderDetailData)
             throws DataNotFoundException;
+
     void deleteById(Long id);
+
     List<OrderDetail> findByOrderId(Long orderId);
-
-
 }
+
